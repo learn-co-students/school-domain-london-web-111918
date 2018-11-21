c6d5330a40727df739a8f64dfb5dbd1ea0983f57
Janine Luk <janineluk95@gmail.com>
2018-11-21 16:01:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+
+class School
+  
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  
+  def school_name
+    @school_name
+  end
+end
+
+school = School.new("Bayside High School")
